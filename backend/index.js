e4d0f338d9b42e73f6469ea0941556a4589fc4b3
kamlMehra <kamalmehra66160@gmail.com>
2024-08-26 10:17:51 +0530

    Final Todo Done

--- backend/index.js
+++ backend/index.js
@@ -8,23 +8,26 @@ import ConnectDB from './DB/index.js'
 
 import route from './routes/Task.routes.js'
 
-import express from 'express'
+import express from "express";
 
-import cors from "cors"
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(cors({
+app.use(bodyParser.json());
+
+app.use(
+  cors({
     origin: "*",
-}));
+  })
+);
 
-const port=process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-ConnectDB().then(()=>{
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`)
-    })
-})
+ConnectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+});
 
-app.use('/api/todo/',route)
+app.use("/api/todo/", route);
